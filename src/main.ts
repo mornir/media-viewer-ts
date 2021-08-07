0cd5086ecbc1d6415be724bceff4e10262384343
mornir <jeromepott@hotmail.com>
2021-08-07 21:05:49 +0200

    Add video

--- src/main.ts
+++ src/main.ts
@@ -2,40 +2,69 @@ import lightGallery from "lightgallery"
 import "lightgallery/css/lightgallery.css"
 import "lightgallery/css/lg-zoom.css"
 import "lightgallery/css/lg-fullscreen.css"
+import "lightgallery/css/lg-video.css"
 // Plugins
 import FullScreen from "lightgallery/plugins/fullscreen"
 import lgZoom from "lightgallery/plugins/zoom"
+import Video from "lightgallery/plugins/video"
 
 const loadImagesButton = document.querySelector("#load-images")!
 const galleryElement = document.getElementById("gallery")!
 
-/* function openGallery() {
-  
+galleryElement.addEventListener("lgSlideItemLoad", (slide) => {
+  // Free up memory
+  URL.revokeObjectURL(slide.detail.index)
+})
+
+async function openGallery() {
+  try {
+    const dirHandle = await window.showDirectoryPicker()
+
+    const images = []
+
+    for await (const value of dirHandle.values()) {
+      const file = await value.getFile()
+
+      console.log(file)
+
+      if (file.type === "image/jpeg") {
+        const objectUrl = URL.createObjectURL(file)
+        images.push({ src: objectUrl })
+      }
+
+      if (file.type === "video/webm") {
+        const objectUrl = URL.createObjectURL(file)
+        images.push({
+          video: {
+            source: [{ src: objectUrl, type: "video/webm" }],
+            attributes: { preload: false, controls: true },
+          },
+        })
+      }
+    }
+
+    const gallery = createGallery(images)
+
+    gallery.openGallery()
+  } catch (error) {
+    console.error(error)
+  }
 }
- */
-function openGallery() {
-  const gallery = lightGallery(galleryElement, {
+
+function createGallery(media) {
+  return lightGallery(galleryElement, {
     dynamic: true,
-    plugins: [lgZoom, FullScreen],
+    plugins: [lgZoom, FullScreen, Video],
     closeOnTap: false,
-    speed: 500,
+    autoplayVideoOnSlide: true,
     counter: false,
     download: false,
     mousewheel: true,
     loop: false,
     hideControlOnEnd: true,
-    dynamicEl: [
-      // @ts-ignore
-      {
-        src: "https://images.unsplash.com/photo-1581894158358-5ecd2c518883?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1406&q=80",
-      },
-      // @ts-ignore
-      {
-        src: "https://images.unsplash.com/photo-1544550285-f813152fb2fd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80",
-      },
-    ],
+
+    dynamicEl: media,
   })
-  gallery.openGallery()
 }
 
 loadImagesButton.addEventListener("click", openGallery)
