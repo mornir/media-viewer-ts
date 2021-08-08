73837289d121db3792d6ffa95081c0b37d2c19f2
mornir <jeromepott@hotmail.com>
2021-08-08 12:01:54 +0200

    feat: add some styling

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  jit: true,
+  purge: ["./index.html", "./src/**/*.{js,ts}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      lato: "'Lato', serif",
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
